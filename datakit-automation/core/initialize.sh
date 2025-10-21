92e88245b991233fc2bef50a7f4134f7543ccdc4
benjamin <benjamin@guance.com>
2025-10-21 21:41:15 +0800

    fix skip task

--- datakit-automation/core/initialize.sh
+++ datakit-automation/core/initialize.sh
@@ -35,7 +35,7 @@ check_running_instance() {
     # 如果是command_list 中的命令，则需要判断是否存在与当前进程不一样pid的datakit_auto_installer.sh进程，如果存在则退出
     if [[ " ${skip_command_list[@]} " =~ " $command " ]]; then
         # 如果存在与当前进程不一样pid的datakit_auto_installer.sh进程，则退出
-        if [ $(pgrep -u datakit -f "datakit_auto_installer.sh" | grep -v $$ | wc -l) -gt 1 ]; then
+        if [ $(pgrep -u datakit -f "datakit_auto_installer.sh" | grep -v $$ | wc -l) -gt 2 ]; then
             handle_error "COMMAND_ERROR" "当前存在主函数正在执行（$command），跳过任务" "WARNING" "true"
         fi
 
